c28fe9e7629f8c7b798fadf0671ecacce16227fc
DillingerB <bennettdv@vcu.edu>
2026-04-11 01:14:39 -0400

    just, a change

--- Db.js
+++ Db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   host:               process.env.DB_HOST     || "localhost",
   port:               parseInt(process.env.DB_PORT || "3306"),
   user:               process.env.DB_USER     || "root",
-  password:           process.env.DB_PASSWORD || "08Dec02*",
+  password:           process.env.DB_PASSWORD || "",
   database:           process.env.DB_NAME     || "medical_ai_demo",
   waitForConnections: true,
   connectionLimit:    10,
